c10e65433d7458a73a342dc8568727ed92f5a07c
MikeCeron <92060085+MikeCeron@users.noreply.github.com>
2021-11-04 16:05:33 -0600

    JavaScript funcional con cambios en contactanos.html

--- src/js/contacto-validacion.js
+++ src/js/contacto-validacion.js
@@ -1,54 +1,119 @@
-// importar formulario
-let form_1 = document.getElementById("form_1");
-// evento presionar boton submit
-form_1.addEventListener("submit", function(e){
-    e.preventDefault();
-    //posibles respuestas
-    var posibleTexto=/^[a-zA-ZÑñÁáÉéÍíÓóÚúÜü\s]+$/;
-    var posibleNumero=/^[0-9]+$/;
-    var posibleCorreo=/^([\da-z_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/
-    //importar entradas
-    let input_nombre = document.getElementById("nombre");
-    let input_correo = document.getElementById("correo");
-    let input_telefono = document.getElementById("teléfono");
-    let input_asunto = document.getElementById("asunto");
-    let input_mensaje = document.getElementById("mensaje");
+
+function validar1(){
     //validaciones nombre
-    if(input_nombre.length > 50 || input_nombre.length < 0){
-        alert("Error, el tamaño del nombre no es correcto");
-    }
-    // if(!posibleTexto.exec(input_nombre.value)){
-        if(!posibleTexto.exec(input_nombre.value)){
+    
+    let input_nombre = document.getElementById("nombre").value;
+
+    var posibleTexto=/^[a-zA-ZÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+
+    if(!posibleTexto.exec(input_nombre.value)){
         alert("El nombre solo puede contener letras y espacios");
-        input_nombre.classList.add("is-invalid");
     }
-    //validación correo
-    if(input_correo.length > 40 || input_correo.length < 10){
+
+}
+
+
+function validar2(){
+
+    let input_correo = document.getElementById("mail").value;
+    var posibleTexto=/^[a-zA-ZÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+    var posibleCorreo=/^([\da-z_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/
+
+     //validación correo
+     if(input_correo.length>40||input_correo.length<10){
         alert("Error, el tamaño del correo no es correcto");
+        return true;
     }
-    if(!posibleCorreo.exec(input_correo.value)){
+    if(!posibleCorreo.exec(input_correo)){
         alert("El correo no tiene el formato esperado");
+        return true;
     }
+    
+
+}
+
+
+function validar3() {
+
+    var posibleNumero=/^[0-9]+$/;
+    let input_telefono = document.getElementById("teléfono").value;
+
     //validaciones teléfono
-    if(input_telefono.length > 10 || input_telefono.length < 8){
+    if(input_telefono.length > 10||input_telefono.length<8){
         alert("Error, el tamaño del teléfono no es correcto");
+        return true;
     }
-    if(!input_telefono.value.match(posibleNumero)){
+    if(!input_telefono.match(posibleNumero)){
         alert("El teléfono solo puede contener numeros");
+        return true;
     }
+    
+}
+
+function validar4(){
     //validaciones asunto
-    if(input_asunto.length > 30 ||input_asunto.length<0){
+    var posibleTexto=/^[a-zA-ZÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+
+    let input_asunto = document.getElementById("asunto1").value;
+
+    if(input_asunto.length > 30 || input_asunto.length<0){
         alert("Error, el tamaño del asunto no es correcto");
+        return true;
     }
     if(!posibleTexto.exec(input_asunto.value)){
         alert("El asunto solo puede contener letras y espacios");
+        return true;
     }
+
+    
+}
+
+
+function validar5(){
+
+    var posibleTexto=/^[a-zA-ZÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+    let input_mensaje = document.getElementById("texto").value;
+    
     //validaciones mensaje
     if(input_mensaje.length > 100 ||input_mensaje.length<0){
         alert("Error, el tamaño del mensaje no es correcto");
+        return true;
     }
     if(!posibleTexto.exec(input_mensaje.value)){
         alert("El asunto solo puede contener letras y espacios");
+        return true;
     }
-});
+}
+
+-
+
+function crearlink(){
+
+
+    if (validar1()==true || validar2() ==true || validar3() ==true || validar4()==true ||  validar5() ==true  ) {
+
+        alert(" Acompleta los campos");
+        
+    }
+    else {
+        var input_correo = document.getElementById("mail").value;
+        var input_nombre = document.getElementById("nombre").value;
+        var texto1 =document.getElementById("texto").value;
+        var asunto3 = document.getElementById("asunto1").value
+
+        let anclacorreo=document.getElementById("correo");
+        anclacorreo.href =`mailto:jart2607@gmail.com?&subject=${asunto3}&body=De parte ${input_nombre}
+         con direccion de email:${input_correo}
+         nos dice que-${texto1} `;
+    }
+    
+
+    
+}
+
+
+
+
+
+
 
