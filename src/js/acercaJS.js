568f63cf829f1ca8e4325fd95c422442bdacce31
jart2222 <91762006+jart2222@users.noreply.github.com>
2021-11-06 18:22:13 -0600

    finalice acerca

--- src/js/acercaJS.js
+++ src/js/acercaJS.js
@@ -4,15 +4,20 @@ class person{
   email= "";
   age=18;
   static total=0
-  constructor(name,email,age,colaboracion,url){
+  constructor(name,email,age,colaboracion,url,linkgit){
       this.name=name;
       this.email = email;
       this.age = age;
       this.colaboracion = `${colaboracion}`;
-      this.url=url
+      this.url=url;
+      this.linkgit=linkgit
       person.total+=1
       this.id=person.total
   }
+
+
+  
+
   printInfo(div){
       div.innerHTML= `<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
       <div class="modal-dialog">
@@ -27,13 +32,18 @@ class person{
             <div class="text-center">
                 <img src=${this.url} class="rounded" alt="imagen${this.id}">
             </div>
-                <div class="alert alert-primary" role="alert">
-                    Correo Electronico: ${this.email}
-                </div>
+              <div class="alert alert-dark" id="alert" role="alert">
+                  Correo Electronico: ${this.email}
+              </div>
             ${this.colaboracion}
           </div>
           <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <div class="btn-group" role="group" aria-label="Basic example">
+              <a href="${this.linkgit}" class="btn btn-primary btn-lg" role="button" aria-disabled="true"><i class="bi bi-github"></i> </a>
+              <a  href="mailto:${this.email}?&" class="btn btn-primary btn-lg " role="button" aria-disabled="true"><i class="bi bi-envelope"></i></a>
+              <a class="btn btn-primary btn-lg disabled" role="button" aria-disabled="true"><i class="bi bi-linkedin"></i> </a>
+              <button type="button" class="btn btn-danger"  aria-disabled="true" data-dismiss="modal">Cerrar</button>
+            </div>
           </div>
         </div>
       </div>
@@ -43,15 +53,13 @@ class person{
   }
 
 
-
-
 function idetificarclase(a){
 if(a==1){
     let divP=document.getElementById("modal1")
     let Armando=new person("Jose Armando Roman Torres","jart2607@gmail.com", 22,
   `Desarrollador Javafull Stack, licenciado en Tecnologia, colaboracion en un proyecto de Automatas 
   celulares con Python, dominio  de HTML, CSS, Boostrap y Javascript  `,
-    "../src/images/PérezElizondo.jpg")
+    "../src/images/PérezElizondo.jpg" ,"https://github.com/jart2222")
     Armando.printInfo(divP);
 }
 else if(a==2){
@@ -59,7 +67,7 @@ else if(a==2){
     let Daisy=new person("Daisy Sarahí Matus Morales","daisymatus54@gmail.com ", 22,
     `Desarrolladora Java Full Stack.			
     Competente en el manejo de diversas plataformas digitales con intereses en desarrollo, cyber security y temas ambientales. Muestra habilidades sociales para el trabajo colaborativo e interés por participar activamente en la solución de problemáticas actuales. `,
-    "../src/images/DaisySarahí.jpg")
+    "../src/images/DaisySarahí.jpg","https://github.com/dmatmo")
     Daisy.printInfo(divP);
 }
 else if(a==3){
@@ -68,14 +76,14 @@ else if(a==3){
     `Desarrollador Java Full Stack y estudiante de Ing. de software.
     Profesional con habilidades informáticas capaz de trabajar Front End con HTML5, CSS3, JavaScript, Bootstrap 4.6 y 5.1  y Backend con Java. VCS Git y GitHub
     Habilidades en la resolución de problemas, trabajo en equipo, organizado, responsable, gestión del tiempo y estrés, con actitud siempre positiva.
-    `,"../src/images/OscarMartínez.jpg")
+    `,"../src/images/OscarMartínez.jpg","https://github.com/OscarMtz1111")
     Oscar.printInfo(divP);
 }
 else if(a==4){
     let divP=document.getElementById("modal1")
     let Saul=new person("Pérez Elizondo Saúl Octavio","octavioelizondo98@gmail.com", 22,
     ` Recién egresado, acostumbrado a trabajar en equipo, a familiarizarme con nuevos conceptos rápidamente y decidido a especializarme en las tecnologías más recientes en el desarrollo de software.  `,
-    "../src/images/PérezElizondo.jpg")
+    "../src/images/PérezElizondo.jpg","https://github.com/ElizOct98")
     Saul.printInfo(divP);
 
 }
@@ -95,15 +103,8 @@ else if(a==6){
     Autodidacta y organizada. 
     Cuenta con una minuciosa atención al detalle para poder innovar en el mundo de la tecnología. 
     `,
-    "../src/images/RodrigoNiebla.jpg")
+    "../src/images/RodrigoNiebla.jpg","https://github.com/iRoooooo")
     Daisy.printInfo(divP);
-}
-else if(a==7){
-    let divP=document.getElementById("modal1")
-    let Oscar=new person("Masyell De la Rosa","jart2607@gmail.com", 22,
-    "Officia est cillum incididunt adipisicing consequat amet cupidatat fugiat est dolor ea aute proident.",
-    "../src/images/PérezElizondo.jpg")
-    Oscar.printInfo(divP);
 }
 else if(a==8){
     let divP=document.getElementById("modal1")
@@ -111,7 +112,7 @@ else if(a==8){
     `Ingeniero en geofísica, aficionado a la programación y a la naturaleza. Me encanta 
     conocer nuevos lugares. Busco desarrollarme profesionalmente en el desarrollo web y 
     también aplicar mis conocimientos en programación en la prospección sísmica.`
-    ,"../src/images/MiguelAngel.jpg")
+    ,"../src/images/MiguelAngel.jpg","https://github.com/MikeCeron")
     miguel.printInfo(divP);
 
 }
@@ -120,26 +121,3 @@ else if(a==8){
 
 }
 
-function crearlink(){
-
-
-  if (validar1()==true || validar2() ==true || validar3() ==true || validar4()==true ||  validar5() ==true  ) {
-
-      alert(" Acompleta los campos");
-      
-  }
-  else {
-      var input_correo = document.getElementById("mail").value;
-      var input_nombre = document.getElementById("nombre").value;
-      var texto1 =document.getElementById("texto").value;
-      var asunto3 = document.getElementById("asunto1").value
-
-      let anclacorreo=document.getElementById("correo");
-      anclacorreo.href =`mailto:jart2607@gmail.com?&subject=${asunto3}&body=De parte ${input_nombre}
-       con direccion de email:${input_correo}
-       nos dice que-${texto1} `;
-  }
-  
-
-  
-}
