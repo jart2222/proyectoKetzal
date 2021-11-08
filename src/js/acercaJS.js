f4a36a8dbf935af39e0f8dfe83c71f9fa9d70cbc
jart2222 <91762006+jart2222@users.noreply.github.com>
2021-11-07 21:01:27 -0600

    linkedin
    Agregue usuarios de linkedin
    nota:
    -Falta foto Armando Roman xD
    -linkedin de Fernando y Mike

--- src/js/acercaJS.js
+++ src/js/acercaJS.js
@@ -4,13 +4,14 @@ class person{
   email= "";
   age=18;
   static total=0
-  constructor(name,email,age,colaboracion,url,linkgit){
+  constructor(name,email,age,colaboracion,url,linkgit,liklimkding){
       this.name=name;
       this.email = email;
       this.age = age;
       this.colaboracion = `${colaboracion}`;
       this.url=url;
-      this.linkgit=linkgit
+      this.linkgit=linkgit;
+      this.liklimkding=liklimkding;
       person.total+=1
       this.id=person.total
   }
@@ -29,8 +30,8 @@ class person{
             </button>
           </div>
           <div class="modal-body">
-            <div class="text-center">
-                <img src=${this.url} class="rounded" alt="imagen${this.id}">
+            <div class="text-center" >
+                <img src=${this.url} class=" img-fluid rounded" alt="imagen${this.id}">
             </div>
               <div class="alert alert-dark" id="alert" role="alert">
                   Correo Electronico: ${this.email}
@@ -41,7 +42,7 @@ class person{
             <div class="btn-group" role="group" aria-label="Basic example">
               <a href="${this.linkgit}" class="btn btn-primary btn-lg" role="button" aria-disabled="true"><i class="bi bi-github"></i> </a>
               <a  href="mailto:${this.email}?&" class="btn btn-primary btn-lg " role="button" aria-disabled="true"><i class="bi bi-envelope"></i></a>
-              <a class="btn btn-primary btn-lg disabled" role="button" aria-disabled="true"><i class="bi bi-linkedin"></i> </a>
+              <a  href="${this.liklimkding}" class="btn btn-primary btn-lg" role="button" aria-disabled="true"><i class="bi bi-linkedin"></i> </a>
               <button type="button" class="btn btn-danger"  aria-disabled="true" data-dismiss="modal">Cerrar</button>
             </div>
           </div>
@@ -59,7 +60,7 @@ if(a==1){
     let Armando=new person("Jose Armando Roman Torres","jart2607@gmail.com", 22,
   `Desarrollador Javafull Stack, licenciado en Tecnologia, colaboracion en un proyecto de Automatas 
   celulares con Python, dominio  de HTML, CSS, Boostrap y Javascript  `,
-    "../src/images/PérezElizondo.jpg" ,"https://github.com/jart2222")
+    "../src/images/PérezElizondo.jpg" ,"https://github.com/jart2222", "https://www.linkedin.com/in/jos%C3%A9-armando-rom%C3%A1n-torres-b3aa78221/")
     Armando.printInfo(divP);
 }
 else if(a==2){
@@ -67,7 +68,7 @@ else if(a==2){
     let Daisy=new person("Daisy Sarahí Matus Morales","daisymatus54@gmail.com ", 22,
     `Desarrolladora Java Full Stack.			
     Competente en el manejo de diversas plataformas digitales con intereses en desarrollo, cyber security y temas ambientales. Muestra habilidades sociales para el trabajo colaborativo e interés por participar activamente en la solución de problemáticas actuales. `,
-    "../src/images/DaisySarahí.jpg","https://github.com/dmatmo")
+    "../src/images/DaisySarahí.jpg","https://github.com/dmatmo" , "https://www.linkedin.com/in/daisy-matus-morales-190042222/")
     Daisy.printInfo(divP);
 }
 else if(a==3){
@@ -76,14 +77,14 @@ else if(a==3){
     `Desarrollador Java Full Stack y estudiante de Ing. de software.
     Profesional con habilidades informáticas capaz de trabajar Front End con HTML5, CSS3, JavaScript, Bootstrap 4.6 y 5.1  y Backend con Java. VCS Git y GitHub
     Habilidades en la resolución de problemas, trabajo en equipo, organizado, responsable, gestión del tiempo y estrés, con actitud siempre positiva.
-    `,"../src/images/OscarMartínez.jpg","https://github.com/OscarMtz1111")
+    `,"../src/images/OscarMartínez.jpg","https://github.com/OscarMtz1111", "https://www.linkedin.com/in/oscarmtz1111/" )
     Oscar.printInfo(divP);
 }
 else if(a==4){
     let divP=document.getElementById("modal1")
     let Saul=new person("Pérez Elizondo Saúl Octavio","octavioelizondo98@gmail.com", 22,
     ` Recién egresado, acostumbrado a trabajar en equipo, a familiarizarme con nuevos conceptos rápidamente y decidido a especializarme en las tecnologías más recientes en el desarrollo de software.  `,
-    "../src/images/PérezElizondo.jpg","https://github.com/ElizOct98")
+    "../src/images/PérezElizondo.jpg","https://github.com/ElizOct98", "https://www.linkedin.com/in/saul-octavio-perez-elizondo/")
     Saul.printInfo(divP);
 
 }
@@ -92,7 +93,7 @@ else if(a==5){
     let Armando=new person("Luis Fernando Melchor Hernández","luisfernandomelchorhernandez@gmail.com", 22,
     `Sociólogo con interés en la programación, a pesar de no contar con una carrera afin, mi determinación me ha permitido aprender las herramientas y habilidades necesarias para ser un desarrollador Java full Stack.
     `,
-    "../src/images/LuisFernando.jpg")
+    "../src/images/LuisFernando.jpg","#")
     Armando.printInfo(divP);
 }
 else if(a==6){
@@ -103,7 +104,7 @@ else if(a==6){
     Autodidacta y organizada. 
     Cuenta con una minuciosa atención al detalle para poder innovar en el mundo de la tecnología. 
     `,
-    "../src/images/RodrigoNiebla.jpg","https://github.com/iRoooooo")
+    "../src/images/RodrigoNiebla.jpg","https://github.com/iRoooooo","https://www.linkedin.com/in/rod-torres-730aa5221")
     Daisy.printInfo(divP);
 }
 else if(a==8){
@@ -112,7 +113,7 @@ else if(a==8){
     `Ingeniero en geofísica, aficionado a la programación y a la naturaleza. Me encanta 
     conocer nuevos lugares. Busco desarrollarme profesionalmente en el desarrollo web y 
     también aplicar mis conocimientos en programación en la prospección sísmica.`
-    ,"../src/images/MiguelAngel.jpg","https://github.com/MikeCeron")
+    ,"../src/images/MiguelAngel.jpg","https://github.com/MikeCeron", "#")
     miguel.printInfo(divP);
 
 }
