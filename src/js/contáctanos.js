3ebb243b4ee958bb7ec419710f31e3197dc46174
Saúl Octavio <octavioelizondo98@gmail.com>
2021-11-23 00:22:35 -0600

    Agregar NavBar a Index y productos

--- src/js/contáctanos.js
+++ src/js/contáctanos.js
@@ -1,41 +1,28 @@
+//posibles respuestas
+let posibleTexto=/^[a-zA-ZÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+let posibleCorreo=/^([\da-z_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+let posibleNumero=/^[0-9]+$/;
+//cargar inputs
+let input_nombre = document.getElementById("nombre").value;
+let input_correo = document.getElementById("mail").value;
 
-function validar1(){
-    //validaciones nombre
-    
-    let input_nombre = document.getElementById("nombre").value;
-
-    var posibleTexto=/^[a-zA-ZÑñÁáÉéÍíÓóÚúÜü\s]+$/;
-
-    if(!posibleTexto.exec(input_nombre.value)){
-        alert("El nombre solo puede contener letras y espacios");
-    }
-
+//validación nombre
+if(!posibleTexto.exec(input_nombre.value)){
+    alert("El nombre solo puede contener letras y espacios");
+}
+//validación correo
+if(input_correo.length>40||input_correo.length<10){
+    alert("Error, el tamaño del correo no es correcto");
+}
+if(!posibleCorreo.exec(input_correo)){
+    alert("El correo no tiene el formato esperado");
 }
 
-
-function validar2(){
-
-    let input_correo = document.getElementById("mail").value;
-    var posibleTexto=/^[a-zA-ZÑñÁáÉéÍíÓóÚúÜü\s]+$/;
-    var posibleCorreo=/^([\da-z_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/
-
-     //validación correo
-     if(input_correo.length>40||input_correo.length<10){
-        alert("Error, el tamaño del correo no es correcto");
-        return true;
-    }
-    if(!posibleCorreo.exec(input_correo)){
-        alert("El correo no tiene el formato esperado");
-        return true;
-    }
-    
-
-}
 
 
 function validar3() {
 
-    var posibleNumero=/^[0-9]+$/;
+   
     let input_telefono = document.getElementById("teléfono").value;
 
     //validaciones teléfono
@@ -53,9 +40,7 @@ function validar3() {
 function validar4(){
     //validaciones asunto
     var posibleTexto=/^[a-zA-ZÑñÁáÉéÍíÓóÚúÜü\s]+$/;
-
     let input_asunto = document.getElementById("asunto1").value;
-
     if(input_asunto.length > 30 || input_asunto.length<0){
         alert("Error, el tamaño del asunto no es correcto");
         return true;
@@ -64,14 +49,11 @@ function validar4(){
         alert("El asunto solo puede contener letras y espacios");
         return true;
     }
-
-    
 }
 
 
 function validar5(){
-
-    var posibleTexto=/^[a-zA-ZÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+    let posibleTexto=/^[a-zA-ZÑñÁáÉéÍíÓóÚúÜü\s]+$/;
     let input_mensaje = document.getElementById("texto").value;
     //validaciones mensaje
     if(input_mensaje.length > 100 ||input_mensaje.length<0){
@@ -86,26 +68,19 @@ function validar5(){
 
 
 function crearlink(){
-
     if (validar1()==true || validar2() ==true || validar3() ==true || validar4()==true ||  validar5() ==true  ) {
-
         alert(" Acompleta los campos");
-        
     }
     else {
-        var input_correo = document.getElementById("mail").value;
-        var input_nombre = document.getElementById("nombre").value;
-        var texto1 =document.getElementById("texto").value;
-        var asunto3 = document.getElementById("asunto1").value
-
+        let input_correo = document.getElementById("mail").value;
+        let input_nombre = document.getElementById("nombre").value;
+        let texto1 =document.getElementById("texto").value;
+        let asunto3 = document.getElementById("asunto1").value
         let anclacorreo=document.getElementById("correo");
         anclacorreo.href =`mailto:ketzalcafeteria@gmail.com?&subject=${asunto3}&body=De parte ${input_nombre}
          con direccion de email:${input_correo}
          nos dice que-${texto1} `;
-    }
-    
-
-    
+    }    
 }
 
 
