1ab08e587ead9ae1c8ebaa6f7884b97a33cfba50
luisfernandoMH <luisfernandomelchorhernandez@gmail.com>
2021-11-29 11:57:54 -0600

    Se agregó un alert en la página de Administrador

--- src/js/administrador.js
+++ src/js/administrador.js
@@ -1,3 +1,18 @@
+function alertUser(mensajealert,color){
+    let alerta=document.getElementById("alerta"); 
+    alerta.innerHTML= `<svg xmlns="http://www.w3.org/2000/svg" style="display: none; ">
+                            <symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
+                              <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                            </symbol>
+                        </svg>
+                        <div class="alert alert-success d-flex align-items-center" ${color}  role="alert">
+                           <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg>
+                          <div style="color: #623307;">
+                            ${mensajealert}
+                          </div>
+                        </div>`
+}
+
 document.addEventListener('DOMContentLoaded', e => { 
     //importar productos default
     let totalProductos = JSON.parse(localStorage.getItem("catalogo"));   
@@ -20,53 +35,75 @@ document.addEventListener('DOMContentLoaded', e => {
         let input_descripcion = document.getElementById("descripcion").value;
         //validaciones nombre
         if(input_nombre.length > 50||input_nombre.length<0){
-            alert("Error, el nombre es muy largo");
+            mensajealert="Error, el nombre es muy largo";
             validar+=1;
+            color="warning"
+            alertUser(mensajealert,color);
         }
         if(!input_nombre.match(posibleNombre)){
-            alert("El nombre no tiene el formato esperado");
+            mensajealert="El nombre no tiene el formato esperado";
             validar+=1;
+            color="warning"
+            alertUser(mensajealert,color);
         }
         //validación precio
         if(input_precio.length>10||input_precio.length<1){
-            alert("Error, el valor del precio no es correcto");
+            mensajealert="Error, el valor del precio no es correcto";
             validar+=1;
+            color="warning"
+            alertUser(mensajealert,color);
         }
         if(!input_precio.match(posibleNumero)){
-            alert("El precio no tiene el formato esperado");
+            mensajealert="El precio no tiene el formato esperado";
             validar+=1;
+            color="warning"
+            alertUser(mensajealert,color);
         }
         //validacion id 
         if(!input_idProducto.match(posibleID)){
-            alert("El ID no tiene el formato esperado");
+            mensajealert="El ID no tiene el formato esperado";
             validar+=1;
+            color="warning"
+            alertUser(mensajealert,color);
         }
         if(totalProductos.hasOwnProperty(input_idProducto)) {
-            alert("ya existe el ID");
+            mensajealert="ya existe el ID";
             validar+=1;
+            color="warning"
+            alertUser(mensajealert,color);
         }
         //validaciones cantidad
         if(input_cantidad.length > 12||input_cantidad.length<1){
-            alert("Error, el valor de la cantidad no es correcto");
+            mensajealert="Error, el valor de la cantidad no es correcto";
             validar+=1;
+            color="warning"
+            alertUser(mensajealert,color);
         }
         if(!input_cantidad.match(posibleNumero)){
-            alert("La cantidad no tiene el formato esperado");
+            mensajealert="La cantidad no tiene el formato esperado";
             validar+=1;
+            color="warning"
+            alertUser(mensajealert,color);
         }
         //validaciones imagen
         if(input_imagen.length > 30 ||input_imagen.length<0){
-            alert("Error, el tamaño del nombre de la imagen no es correcto");
+            mensajealert="Error, el tamaño del nombre de la imagen no es correcto";
             validar+=1;
+            color="warning"
+            alertUser(mensajealert,color);
         }
         if(!input_imagen.match(posibleNombre)){
-            alert("Solo escriba el nombre de la imagen, sin la extención o ubicación");
+            mensajealert="Solo escriba el nombre de la imagen, sin la extención o ubicación";
             validar+=1;
+            color="warning"
+            alertUser(mensajealert,color);
         }
         //validaciones descripción
         if(input_descripcion.length > 100 ||input_descripcion.length<0){
-            alert("Error, el tamaño de la descripción no es correcto");
+            mensajealert="Error, el tamaño de la descripción no es correcto";
             validar+=1;
+            color="warning"
+            alertUser(mensajealert,color);
         }
         if (validar==0){
             totalProductos[input_idProducto] = {
