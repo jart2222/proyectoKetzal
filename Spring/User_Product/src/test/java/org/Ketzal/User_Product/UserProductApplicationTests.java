15b2816a2d3fee69e2f8d616fcc98eb7b97e57de
Saúl Octavio <octavioelizondo98@gmail.com>
2021-12-01 10:28:05 -0600

    Spring web

---
+++ Spring/User_Product/src/test/java/org/Ketzal/User_Product/UserProductApplicationTests.java
@@ -0,0 +1,13 @@
+package org.Ketzal.User_Product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
