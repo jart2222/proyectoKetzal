f5007b9323bb2348d3d75f17dba80568b5fc813c
jart2222 <91762006+jart2222@users.noreply.github.com>
2021-12-09 12:26:29 -0600

    Correccion modal productos
    Corregí un error en la pagina de administrador que no corregía el modal con  los datos de cada id. también implemente PUT de productos

--- src/js/adminEditar.js
+++ src/js/adminEditar.js
@@ -38,74 +38,81 @@ function crear_lista(idP,nombreP,imagenP,productoP,precioP,gramajeP){
                                 <li class="list-group-item list-group-item-danger">Peso: ${gramajeP} gr</li>
                                 <li class="list-group-item list-group-item-danger"><button type="button" class="btn btn-success" onclick="borrarProducto(${idP})" >Eliminar</button></li>
                                 <li class="list-group-item list-group-item-danger">
-                                    <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                    <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#exampleModal${idP}">
                                         Editar
                                     </button>
                                 </li>
                             </ul> 
-                            <!-- Button trigger modal -->
-                            
+                        <div>  
 
-                            <!-- Modal -->
-                            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                            <div class="modal-dialog modal-xl">
-                                <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="exampleModalLabel">Editor de Productos</h5>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                </div>
 
-                                <div class="modal-body">
+                        
+                        <div class="modal fade" id="exampleModal${idP}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                            <div class="modal-dialog modal-xl">
+                                                            <div class="modal-content">
+                                                                <div class="modal-header">
+                                                                <h5 class="modal-title" id="exampleModalLabel">Editor de Producto con Id ${idP}</h5>
+                                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                                </div>
+                                                                <div class="modal-body">
 
-                                    <section id="form_1" class="container-fluid">
-                                        <div class="row justify-content-center">
-                                        <div class="imagen">
-                                            <h1>ADMINISTRADOR</h1>
-                                        </div>
-                                        <div class="row" style=" padding: 7%;">
-                                            <form class="row g-3">
-                                            <div id="alerta">
-                                                
-                                            </div>
-                                            <div class="col-md-6">
-                                                <label for="producto" class="form-label">NOMBRE DEL PRODUCTO *</label>
-                                                <input id="nombreU${idP}" type="text" class="form-control" minlength="1" maxlength="30" value="${nombreP}" >
-                                            </div>
-                                            <div class="col-md-6">
-                                                <label for="idProducto" class="form-label">ID DEL PRODUCTO *</label>
-                                                <input type="number" step="1" min="1" class="form-control"  value="${idP}"  disabled id="idProductoU${idP}">
-                                            </div>
-                                            <div class="col-md-6">
-                                                <label for="precio" class="form-label">PRECIO *</label>
-                                                <input type="number"  step="0.01" min="1.0" class="form-control" value="${precioP}"  id="precioU${idP}">
-                                            </div>
-                                            <div class="col-md-6">
-                                                <label for="Cantidad" class="form-label">CANTIDAD EN GRAMOS *</label>
-                                                <input type="number" step="0.01" min="1.0" class="form-control" value="${gramajeP}" id="cantidadU${idP}">
-                                            </div>    
-                                            <div class="mb-3">
-                                                <label for="Imagen" class="form-label">DIRECCION DE LA IMAGEN *</label>
-                                                <input type="text" minlength="1" maxlength="100" class="form-control" value="${imagenP}" id="imagenU${idP}">
-                                            </div>
-                                            <div class="mb-3">
-                                                <label for="Descripción" class="form-label">DESCRIPCIÓN DEL PRODUCTO *</label>
-                                                <textarea class="form-control" minlength="1" maxlength="200" id="descripcionU${idP}" type="text" rows="3"> ${productoP}</textarea>
-                                            </div>
-                                        
-                                            </form>
-                                        </div><!--div form -->
-                                        </div><!--div row-->
-                                    </section>
+                                                                <section id="form_1" class="container-fluid">
+                                                                    <div class="row justify-content-center">
+                                                                    <div class="imagen">
+                                                                        <h1>ADMINISTRADOR</h1>
+                                                                    </div>
+                                                                    <div class="row" style=" padding: 7%;">
+                                                                        <form class="row g-3">
+                                                                        <div id="alerta">
+                                                                            
+                                                                        </div>
+                                                                        <div class="col-md-6">
+                                                                            <label for="producto" class="form-label">NOMBRE DEL PRODUCTO *</label>
+                                                                            <input id="nombreU${idP}" type="text" class="form-control" minlength="1" maxlength="30" value="${nombreP}" >
+                                                                        </div>
+                                                                        <div class="col-md-6">
+                                                                            <label for="idProducto" class="form-label">ID DEL PRODUCTO *</label>
+                                                                            <input type="number" step="1" min="1" class="form-control"  value="${idP}"  disabled id="idProductoU${idP}">
+                                                                        </div>
+                                                                        <div class="col-md-6">
+                                                                            <label for="precio" class="form-label">PRECIO *</label>
+                                                                            <input type="number"  step="0.01" min="1.0" class="form-control" value="${precioP}"  id="precioU${idP}">
+                                                                        </div>
+                                                                        <div class="col-md-6">
+                                                                            <label for="Cantidad" class="form-label">CANTIDAD EN GRAMOS *</label>
+                                                                            <input type="number" step="0.01" min="1.0" class="form-control" value="${gramajeP}" id="cantidadU${idP}">
+                                                                        </div>    
+                                                                        <div class="mb-3">
+                                                                            <label for="Imagen" class="form-label">DIRECCION DE LA IMAGEN *</label>
+                                                                            <input type="text" minlength="1" maxlength="100" class="form-control" value="${imagenP}" id="imagenU${idP}">
+                                                                        </div>
+                                                                        <div class="mb-3">
+                                                                            <label for="Descripción" class="form-label">DESCRIPCIÓN DEL PRODUCTO *</label>
+                                                                            <textarea class="form-control" minlength="1" maxlength="200" id="descripcionU${idP}" type="text" rows="3"> ${productoP}</textarea>
+                                                                        </div>
+                                                                    
+                                                                        </form>
+                                                                    </div><!--div form -->
+                                                                    </div><!--div row-->
+                                                                </section>
 
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="submit" onclick="actualizar_boton(${idP})" class="btn justify-content-center">AGREGAR</button>
-                                    <button type="button" class="btn justify-content-center" data-bs-dismiss="modal">Close</button>
-                                </div>
-                                </div>
-                            </div>
-                            </div>         
-                        <div>  `
+
+
+
+
+                                                                </div>
+                                                                <div class="modal-footer">
+                                                                <button type="submit" onclick="actualizar_boton(${idP})" class="btn justify-content-center">AGREGAR</button>
+                                                                <button type="button" class="btn justify-content-center" data-bs-dismiss="modal">Close</button>
+                                                                </div>
+                                                            </div>
+                                                            </div>
+                                                        </div>
+
+                                                                </div>
+                        
+                        
+                        `
     }
 
 
@@ -123,11 +130,12 @@ function borrarProducto(id){
 
 function actualizar_boton(idP){
     let idU=idP
-    let nombreU=document.getElementById(`nombreU${idU}`).value;
-    let imagenU= document.getElementById("imagenU"+idU).value;;
-    let productoU=document.getElementById("descripcionU"+$idU).value;
-    let precioU= document.getElementById("precioU"+idU).value;
-    let gramajeU= document.getElementById("cantidadU"+idU).value;
+    let nombreU=document.getElementById(`nombreU${idP}`).value;
+    console.log(nombreU);
+    let imagenU= document.getElementById(`imagenU${idP}`).value;;
+    let productoU=document.getElementById(`descripcionU${idP}`).value;
+    let precioU= document.getElementById(`precioU${idP}`).value;
+    let gramajeU= document.getElementById(`cantidadU${idP}`).value;
     update_producto(idU,nombreU,imagenU,productoU,precioU,gramajeU)
 
 }
